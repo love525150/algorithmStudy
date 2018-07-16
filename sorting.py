a5bfecdf1a8f7f810b58c7ad9de73f8ae18b3ecd
zhouzhengwen <739971534@qq.com>
2018-07-16 15:06:38 +0800

    更新插入排序

--- sorting.py
+++ sorting.py
@@ -22,10 +22,56 @@ def _switch_list_value(list_, p1, p2):
     list_[p1] = list_[p2]
     list_[p2] = temp
 
+'''
+这个是比较交换的方式排序（其实类似反向冒泡排序）
+'''
+def insertion_sort(sorting_list):
+    for i in range(len(sorting_list)):
+
+        for j in range(i, 0, -1):
+            if sorting_list[j] < sorting_list[j - 1]:
+                _switch_list_value(sorting_list, j, j-1)
+            else :
+                break
+
+'''
+这个是真按照洗牌的方式插入排序
+'''
+def insert_sort_improved_version(sorting_list):
+    for i in range(len(sorting_list)):
+        source_position = i
+        target_position = i
+        current_element = sorting_list[i]
+        
+        for j in range(i, 0, -1):
+            last_sorted_element = sorting_list[j - 1]
+            last_sorted_element_index = j - 1
+            if current_element < last_sorted_element:
+                target_position = last_sorted_element_index
+            else :
+                break
+
+        _insert_list_value(sorting_list, source_position, target_position)    
+                
+
+def _insert_list_value(_list, source_position, target_position):
+    assert source_position >= target_position, "数组元素只允许从后往前插"
+    insert_element = _list[source_position]
+    
+    for i in range(source_position, target_position, -1):
+        _list[i] = _list[i - 1]
+    
+    _list[target_position] = insert_element
+
+
 def test_sort():
-    selection_sort_test_list = [4,66,74,25]
-    selection_sort(selection_sort_test_list)
-    print("selection sort排序结果：" + str(selection_sort_test_list))
+    # selection_sort_test_list = [4,66,74,25]
+    # selection_sort(selection_sort_test_list)
+    # print("selection sort排序结果：" + str(selection_sort_test_list))
+
+    insertion_sort_test_list = [4,66,74,25,125,908,456,9,36]
+    insert_sort_improved_version(insertion_sort_test_list)
+    print("selection sort排序结果：" + str(insertion_sort_test_list))
 
 
 test_sort()
